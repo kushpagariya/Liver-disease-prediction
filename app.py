d3af5ce70cead1f7b421fe12cb5fc4b26ec7c1f5
kushpagariya <yourgithubemail@gmail.com>
2026-03-05 18:44:21 +0530

    import os

--- app.py
+++ app.py
@@ -59,6 +59,6 @@ def predict():
         return jsonify({'error': str(e)})
 import os
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
